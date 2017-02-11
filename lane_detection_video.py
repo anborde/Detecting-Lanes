b748d162acfa93697bd5dfe3718d3cfed4ce1d0a
anborde <adityanborde@gmail.com>
2017-02-11 17:29:35 +0530

    Add files via upload

--- lane_detection_video.py
+++ lane_detection_video.py
@@ -1,7 +1,16 @@
-from video_processing_pipeline import findLanes
+from video_processing_pipeline import findLanes, getVideoSize
 
 """
     Sample code to detct lanes in a video using video_processing_pipeline.py
 """
 
-findLanes("solidWhiteRight.mp4", "white.mp4")
+# Input file
+input_file = "solidWhiteRight.mp4"
+
+# Getting Video Size
+shape = getVideoSize(input_file)
+
+# Defining the vertices for region of interest
+vertices = [[(450, 320),(525, 320), (900, shape[0]), (150, shape[0])]]
+
+findLanes(input_file, "white.mp4", vertices)
